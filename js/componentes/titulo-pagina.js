fbf47eab6c6e2e84e2bf854b52ddf90edd37b469
Albie <albiesilva@gmail.com>
2025-06-25 11:55:52 -0400

    cambio de componente titulo, se modifico estructura para que tomara en cuanto lo estilos globales ya establecidos

--- js/componentes/titulo-pagina.js
+++ js/componentes/titulo-pagina.js
@@ -1,20 +1,18 @@
 class TituloPagina extends HTMLElement{
     constructor(){
         super();
-        const shadow = this.attachShadow({mode: 'open'});
-
-        const titulo = document.createElement('h2');
-        titulo.textContent = this.getAttribute('texto') || 'Urban'; /*encaso que la pagina no tenga titulo toma por defecto el nombre de la tienda */
-        
-        shadow.appendChild(titulo);
-
+        const textoTitulo = this.getAttribute('texto') || 'Urban'; /*si la pagina tiene error o no tiene titulo tomara "Urban" como titulo*/
+        this.innerHTML = `<h2>${textoTitulo}</h2>`;
     }
+
     static get observedAttributes(){
         return['texto'];
     }
+
     attributeChangedCallback(nombre, viejo, nuevo){
-        if(nombre==='texto' && this.shadowRoot){
-            this.shadowRoot.querySelector('h2').textContent = nuevo;
+        if(nombre==='texto' && this.querySelector('h2')){
+            const h2 = this.querySelector('h2');
+            if(h2) h2.textContent = nuevo;
         }
     }
 }
